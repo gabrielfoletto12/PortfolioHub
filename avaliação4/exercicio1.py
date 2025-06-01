4f52003a246042e5e608b9e5bbee07dc6e2ded52
gabrielfoletto12 <gabrielfoletto@sempreceub.com>
2025-06-01 19:47:03 -0300

    projetos_academicos

---
+++ avaliação4/exercicio1.py
@@ -0,0 +1,5 @@
+contador = 0
+for i in range(1, 14, 2):
+    print(i)
+    contador += 1
+print("Quantidade:", contador)
---
+++ avaliação4/exercicio4.py
@@ -0,0 +1,6 @@
+fim = int(input("Digite o valor final: "))
+contador = 0
+for i in range(1, fim + 1):
+    print(i)
+    contador += 1
+print("Quantidade:", contador)
